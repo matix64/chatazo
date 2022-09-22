f312199a2faa6a61447f8cfc37c21bb79541065c
Matias <matias.pz@proton.me>
2022-09-22 05:41:36 -0300

    validate messages

--- server/src/messages/models/message-section.dto.ts
+++ server/src/messages/models/message-section.dto.ts
@@ -1,40 +1,43 @@
-import { TypeOptions } from 'class-transformer';
-import { IsIn, IsNotEmpty, IsUrl } from 'class-validator';
+import { TypeOptions } from "class-transformer";
+import { IsIn, IsNotEmpty, IsOptional, IsUrl, Length } from "class-validator";
 
 export class MessageSectionDto {
-  type: 'text' | 'image' | 'event';
+  type: "text" | "image" | "event";
 }
 
 export class UserMessageSectionDto extends MessageSectionDto {
-  @IsIn(['text', 'image'])
-  type: 'text' | 'image';
+  @IsIn(["text", "image"])
+  type: "text" | "image";
 }
 
 export class TextSectionDto extends UserMessageSectionDto {
-  type: 'text';
+  type: "text";
   @IsNotEmpty()
   content: string;
+  @IsOptional()
+  @Length(1, 48)
   format: string;
 }
 
 export class ImageSectionDto extends UserMessageSectionDto {
-  type: 'image';
-  @IsUrl({ protocols: ['http', 'https'] })
+  type: "image";
+  @IsUrl({ protocols: ["http", "https"] })
+  @Length(1, 256)
   url: string;
 }
 
 export class EventSectionDto extends MessageSectionDto {
-  type: 'event';
+  type: "event";
   event: string;
 }
 
 export const UserMessageSectionTypeOptions: TypeOptions = {
   keepDiscriminatorProperty: true,
   discriminator: {
-    property: 'type',
+    property: "type",
     subTypes: [
-      { name: 'text', value: TextSectionDto },
-      { name: 'image', value: ImageSectionDto },
+      { name: "text", value: TextSectionDto },
+      { name: "image", value: ImageSectionDto },
     ],
   },
 };
--- server/src/messages/models/send-message.dto.ts
+++ server/src/messages/models/send-message.dto.ts
@@ -1,14 +1,47 @@
-import { Type } from 'class-transformer';
-import { ArrayNotEmpty, ValidateNested } from 'class-validator';
+import { Type } from "class-transformer";
+import {
+  ArrayNotEmpty,
+  Validate,
+  ValidateNested,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from "class-validator";
+import { unreachable } from "src/util";
 import {
   UserMessageSectionTypeOptions,
   UserMessageSectionDto,
   AnyUserMessageSectionDto,
-} from './message-section.dto';
+} from "./message-section.dto";
+
+@ValidatorConstraint()
+class MessageContentValidator implements ValidatorConstraintInterface {
+  validate(value: AnyUserMessageSectionDto[]): boolean {
+    let totalTextLen = 0;
+    let lastSectionType = "";
+    let imageSections = 0;
+    for (const section of value) {
+      switch (section.type) {
+        case "text":
+          totalTextLen += section.content.length;
+          if (totalTextLen > 2000 || lastSectionType == "text") return false;
+          break;
+        case "image":
+          imageSections++;
+          if (imageSections > 3) return false;
+          break;
+        default:
+          unreachable(section);
+      }
+      lastSectionType = section.type;
+    }
+    return true;
+  }
+}
 
 export class SendMessageDto {
   @ArrayNotEmpty()
   @ValidateNested({ each: true })
   @Type(() => UserMessageSectionDto, UserMessageSectionTypeOptions)
+  @Validate(MessageContentValidator, { message: "Invalid message" })
   content: AnyUserMessageSectionDto[];
 }
